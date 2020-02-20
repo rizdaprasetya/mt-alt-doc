ac1c2416f922c0640f4d6312f291f26ae5f063ca
rizdaprasetya <rizda.prasetya@midtrans.com>
2020-02-20 16:15:08 +0700

    optimize style switcher

--- asset/style-switcher.js
+++ asset/style-switcher.js
@@ -7,61 +7,51 @@
     /**
      * Adds event listeners to change active stylesheet and restore previously
      * activated stylesheet on reload.
-     * @customized:
-     * - Replaced to use `title` attribute for <a> instead of `data-title`
-     * So it can be rendered from markdown e.g `[Foo](# 'style::Foo')`
-     * it require `style::` prefix on the `title` attribute
-     * - Edited to activate all <link> element with target "style::title", 
-     * instead of previously just 1 element. So we can have two or more <link>
-     * activated with just one <a> trigger
      *
-     * @example
+     * as style switch trigger, it require:
+     *    - `<a>` tag
+     *    - With `title` attribute, prefixed with `style::`
+     *    - Put the style name after prefix
+     * as target, it will activate:
+     *    - One or multiple `<link>` tag
+     *    - Which `title` attribute matched the `<a>` tag's `title` attribute
      *
-     * This link:
-     *   <a href="#" title="style::Foo">Foo</a>
-     * Will active all of these existing links:
-     *   <link rel="stylesheet alternate" title="style::Foo" href="..." >
-     *   <link rel="stylesheet alternate" title="style::Foo" href="...other css...">
+     * @example:
+     * This <a> tag:
+     *   <a href="#" title="style::dark">Switch to Dark theme</a>
+     *   - Note: Docsify markdown version = [Switch to Dark theme](# 'style::dark')
+     * Will activate this existing <link> tags:
+     *   <link rel="stylesheet alternate" title="style::dark" href="path/to/dark-style.css" >
+     *   <link rel="stylesheet alternate" title="style::dark" href="path/to/dark-style-2nd-file.css" >
+     * And deactivate these <link> tags with `style::` prefixed title:
+     *   <link rel="stylesheet" title="style::OtherThanDark" href="path/to/OtherThanDark-style.css" >
      *
-     * @example
-     *
-     * This link:
-     *   <a href="#" data-link-href="path/to/file.css">Bar</a>
-     * Will activate this existing link:
-     *   <link rel="stylesheet alternate" title="style::Bar" href="path/to/file.css" >
-     * Or generate this active link:
-     *   <link rel="stylesheet" title="style::Bar" href="path/to/file.css" >
+     * @example usage:
+     * List all the available styles, Light style as default
+     *   <link rel="stylesheet" title="style::light" href="path/to/light-style.css" >
+     *   <link rel="stylesheet alternate" title="style::dark" href="path/to/dark-style.css" >
+     *   <link rel="stylesheet alternate" title="style::dark" href="path/to/dark-style-2nd-file.css" >
+     *   <link rel="stylesheet alternate" title="style::blue" href="path/to/blue-style.css" >
+     * Triggers to switch
+     *   <a href="#" title="style::dark">Switch to Dark theme</a>
+     *   <a href="#" title="style::light">Switch to Light theme</a>
+     *   <a href="#" title="style::blue">Switch to Blue theme</a>
      */
     function initStyleSwitcher() {
         var isInitialzed      = false;
         var sessionStorageKey = 'activeStylesheetHref';
         var titlePrefix = 'style::';
 
-        function handleSwitch(activeHref, activeTitle) {
-            var activeElm = document.querySelector('link[href*="' + activeHref +'"],link[title="' + activeTitle +'"]');
-
-            if (!activeElm && activeHref) {
-                activeElm = document.createElement('link');
-                activeElm.setAttribute('href', activeHref);
-                activeElm.setAttribute('rel', 'stylesheet');
-                activeElm.setAttribute('title', activeTitle);
-
-                document.head.appendChild(activeElm);
-
-                activeElm.addEventListener('load', function linkOnLoad() {
-                    activeElm.removeEventListener('load', linkOnLoad);
-                    setActiveLink(activeElm);
-                });
-            }
-            else if (activeElm) {
-                setActiveLink(activeElm);
-            }
+        function handleSwitch(activeTitle) {
+            var activeElm = document.querySelector('link[title="' + activeTitle +'"]');
+            setActiveLink(activeElm);
         }
 
         function setActiveLink(activeElm) {
             var activeHref   = activeElm.getAttribute('href');
             var activeTitle  = activeElm.getAttribute('title');
-            var inactiveElms = document.querySelectorAll('link[title]:not([href*="' + activeHref +'"]):not([title="' + activeTitle +'"])');
+            var inactiveElms = document.querySelectorAll(
+                'link[title^="' + titlePrefix + '"]:not([href*="' + activeHref +'"]):not([title="' + activeTitle +'"])');
 
             // CUSTOM OVERRIDE to allow all Simple Dark titled CSS to be activated together
             // so now all CSS tag with same activeTitle will all be activated together in group
@@ -76,12 +66,10 @@
             })
 
             // Store active style sheet
-            sessionStorage.setItem(sessionStorageKey, activeHref);
+            sessionStorage.setItem(sessionStorageKey, activeTitle);
 
             // Disable other elms
-            for (var i = 0; i < inactiveElms.length; i++) {
-                var elm = inactiveElms[i];
-
+            inactiveElms.forEach(function(elm){
                 elm.disabled = true;
 
                 // Fix for browsersync and alternate stylesheet updates. Will
@@ -94,7 +82,7 @@
 
                     elm.setAttribute('rel', linkRelAlt);
                 }
-            }
+            });
 
             // CSS custom property ponyfil
             if ((window.$docsify || {}).themeable) {
@@ -108,33 +96,29 @@
 
             // Restore active stylesheet
             document.addEventListener('DOMContentLoaded', function() {
-                var activeHref = sessionStorage.getItem(sessionStorageKey);
+                var activeTitle = sessionStorage.getItem(sessionStorageKey);
 
-                if (activeHref) {
-                    handleSwitch(activeHref);
+                if (activeTitle) {
+                    handleSwitch(activeTitle);
                 }
             });
 
             // Update active stylesheet
             /** 
             * @OPTIMIZE: this will add event listener on all click
-            * Optimize me so that it will only add to link with specific attribute?
             */
             document.addEventListener('click', function(evt) {
-                var dataHref  = evt.target.getAttribute('data-link-href');
-                var dataTitle = evt.target.getAttribute('title')
-                var dataTitleIncludePrefix = dataTitle? 
-                    dataTitle.toLowerCase().includes(titlePrefix):
-                    false;
+                var dataTitle = evt.target.getAttribute('title');
+                if(!dataTitle){ return 0; }
+                var dataTitleIncludePrefix = dataTitle.toLowerCase().includes(titlePrefix);
+                if(!dataTitleIncludePrefix){ return 0; }
 
-                if (dataHref || dataTitleIncludePrefix) {
-                    dataTitle = dataTitle
-                        || evt.target.textContent
-                        || '_' + Math.random().toString(36).substr(2, 9); // UID
+                dataTitle = dataTitle
+                    || evt.target.textContent
+                    || '_' + Math.random().toString(36).substr(2, 9); // UID
 
-                    handleSwitch(dataHref, dataTitle);
-                    evt.preventDefault();
-                }
+                handleSwitch(dataTitle);
+                evt.preventDefault();
             });
         }
     }
