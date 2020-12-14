5970323473ad7b160a0ecea8d0a4ff8c2e31d9b6
egiesem <egiesem99@gmail.com>
2020-12-14 12:25:14 +0700

    update right sidebar

--- asset/revamp/js/custom.js
+++ asset/revamp/js/custom.js
@@ -8,10 +8,33 @@ setTimeout(() => {
   }
   console.log('sidebar',sidebarSearch)
 
-
-  
-
 }, 50);
+
+setTimeout(() => {
+
+  console.log('running')
+  //right sidebar content
+  let sideBarSelector = document.getElementById('right-bar-content')
+  let sideBarContens = ''
+  let getAllTitle = document.querySelectorAll("h2")
+  if(getAllTitle) {
+    console.log('getalltitle', getAllTitle)
+    getAllTitle.forEach(element => {
+      let titleText = element.innerText
+      let titleId = element.id
+      if(titleId) {
+        sideBarContens += '<li class="sidebar__right-list"><a href="?id='+ titleId +'" class="sidebar__right-link sidebar__right-link--active">'+ titleText +'</a></li>'
+      }
+      
+
+    });
+
+    console.log('hasil', sideBarContens)
+    if(sideBarSelector) {
+      sideBarSelector.innerHTML = sideBarContens
+    }
+  }
+}, 150);
 
 
 
