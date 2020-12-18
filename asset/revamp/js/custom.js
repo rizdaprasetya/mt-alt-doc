581bc9a425cb99b440c2adccce0da660620c52ed
egiesem <egiesem99@gmail.com>
2020-12-18 09:40:20 +0700

    add active to rightsidebar menus

--- asset/revamp/js/custom.js
+++ asset/revamp/js/custom.js
@@ -23,72 +23,81 @@ function getRightSideBarContent() {
 
 // for accordion icon (rotate icon) on click
 function accordionLabelTag() {
-  let accordionLabelSelector = document.querySelectorAll('.collaps-label')
-  if( accordionLabelSelector ) {
-    console.log('checked and get collaps')
-    accordionLabelSelector.forEach(element => {
-      element.addEventListener('click', () => {
-        if( element.classList.contains('open') ) {
-          element.classList.remove('open')
-        }else {
-          element.classList.add('open')
-        }
+  setTimeout(() => {
+    let accordionLabelSelector = document.querySelectorAll('.collaps-label')
+    if( accordionLabelSelector ) {
+      console.log('checked and get collaps')
+      accordionLabelSelector.forEach(element => {
+        element.addEventListener('click', () => {
+          if( element.classList.contains('open') ) {
+            element.classList.remove('open')
+          }else {
+            element.classList.add('open')
+          }
+        });
       });
-    });
-  }
+    }
+  }, 50);
 }
 
 // add active to right side menus on scroll
 function rightMenusActiveScroll() {
-  const contents = document.querySelectorAll('div[id^="content-"]');
-  const navLinks = this.$el.querySelectorAll(".nav-link");
 
-  const contentLength = contents.length;
-  window.addEventListener("scroll", function (event) {
-    event.preventDefault();
-    const scrollPos =
-      (window.pageYOffset ||
-        document.documentElement.scrollTop ||
-        document.body.scrollTop ||
-        0) + 100;
-    let contentsTop = [];
-    contents.forEach((content, index) => {
-      contentsTop.push(content.offsetTop);
-    });
+  setTimeout(() => {
+    const contents = document.querySelectorAll('h2[id]');
+    const navLinks = document.querySelectorAll(".sidebar__right-list");
 
-    contentsTop.forEach((contentTop, index) => {
-      if (index + 1 < contentLength) {
-        if (
-          scrollPos > contentTop &&
-          scrollPos < contentsTop[index + 1] &&
-          !navLinks[index].classList.contains("active")
-        ) {
-          navLinks[index].classList.add("active");
-        } else if (
-          scrollPos > contentsTop[index + 1] &&
-          navLinks[index].classList.contains("active")
-        ) {
-          navLinks[index].classList.remove("active");
-        } else if (
-          scrollPos <= contentTop &&
-          navLinks[index].classList.contains("active") &&
-          index !== 0
-        ) {
-          navLinks[index].classList.remove("active");
+    console.log('content', contents)
+    console.log('navLinks', navLinks)
+
+    const contentLength = contents.length;
+    window.addEventListener("scroll", function (event) {
+      event.preventDefault();
+      const scrollPos =
+        (window.pageYOffset ||
+          document.documentElement.scrollTop ||
+          document.body.scrollTop ||
+          0) + 100;
+      let contentsTop = [];
+      contents.forEach((content, index) => {
+        contentsTop.push(content.offsetTop);
+      });
+
+      contentsTop.forEach((contentTop, index) => {
+        if (index + 1 < contentLength) {
+          if (
+            scrollPos > contentTop &&
+            scrollPos < contentsTop[index + 1] &&
+            !navLinks[index].classList.contains("active")
+          ) {
+            navLinks[index].classList.add("active");
+          } else if (
+            scrollPos > contentsTop[index + 1] &&
+            navLinks[index].classList.contains("active")
+          ) {
+            navLinks[index].classList.remove("active");
+          } else if (
+            scrollPos <= contentTop &&
+            navLinks[index].classList.contains("active") &&
+            index !== 0
+          ) {
+            navLinks[index].classList.remove("active");
+          }
+        } else {
+          if (
+            scrollPos > contentTop &&
+            !navLinks[index].classList.contains("active")
+          ) {
+            navLinks[index].classList.add("active");
+          } else if (
+            scrollPos <= contentTop &&
+            navLinks[index].classList.contains("active")
+          ) {
+            navLinks[index].classList.remove("active");
+          }
         }
-      } else {
-        if (
-          scrollPos > contentTop &&
-          !navLinks[index].classList.contains("active")
-        ) {
-          navLinks[index].classList.add("active");
-        } else if (
-          scrollPos <= contentTop &&
-          navLinks[index].classList.contains("active")
-        ) {
-          navLinks[index].classList.remove("active");
-        }
-      }
+      });
     });
-  });
+  }, 170);
+  
 }
